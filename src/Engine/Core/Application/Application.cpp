1603320982569419ee0ac7b6903f2b39a83c2c61
Meth Munindradasa <methsndr@gmail.com>
2023-03-22 09:04:03 +0530

    Proper SDL2 intergration

--- src/Engine/Core/Application/Application.cpp
+++ src/Engine/Core/Application/Application.cpp
@@ -1,4 +1,7 @@
 #include "Application.h"
+#include "SDL_surface.h"
+#include "SDL_video.h"
+#include <SDL.h>
 
 #include <iostream>
 
@@ -16,6 +19,24 @@ namespace AE {
 	{
 		// while (true);
 		std::cout<< "App Executed" << std::endl;
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if(!window)
+      std::cout<<"Failed to create window\n";
+    
+    SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+      std::cout<<"Failed to get surface from window\n";
+
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(5000);
+
 	}
 
 } // namespace AE
